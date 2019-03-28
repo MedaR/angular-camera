f7c973a171de8d53a84a820ed3cc08aa8030d74d
Medahi <relesse.medahi@gmail.com>
2019-03-28 15:12:16 +0100

    fixation pointeur sur consultation

--- src/app/consultation-capteur/consultation-capteur.component.ts
+++ src/app/consultation-capteur/consultation-capteur.component.ts
@@ -18,6 +18,8 @@ export class ConsultationCapteurComponent implements OnInit {
 	capteur = StockCapteur;
   modifier: boolean;
   information: Capteur;
+  myfrugalmap: any;
+  voir: boolean;
   /*
   icon = {
     icon: L.icon({
@@ -36,36 +38,58 @@ export class ConsultationCapteurComponent implements OnInit {
 	ngOnInit() {
 
     // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
-    var myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
-    L.control.scale().addTo(myfrugalmap);
+    this.myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
+    L.control.scale().addTo(this.myfrugalmap);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
-    }).addTo(myfrugalmap);
+    }).addTo(this.myfrugalmap);
 
-    var searchControl = new ELG.Geosearch().addTo(myfrugalmap);
+    //Methode de recherche de ville avec pointeur
+    var test : string = '333';
+     test += "78";
+    console.log("affiche : 111---> "+test);
+    var searchControl = new ELG.Geosearch().addTo(this.myfrugalmap);
 
-    var results = new L.LayerGroup().addTo(myfrugalmap);
-
-      searchControl.on("results", function(data) {
+    var results = new L.LayerGroup().addTo(this.myfrugalmap);
+     console.log("affiche : 11133---> "+L.marker.length);
+      test +="b";
+      searchControl.on(results, function(data) {
+        
+        test +="a";
         results.clearLayers();
           for (var i = data.results.length - 1; i >= 0; i--) {
             results.addLayer(L.marker(data.results[i].latlng));
+            test += data.results[i].latlng;
           }
         });
+      test += "78";
+     
  
+      //Methode récupérer la position d'un capteur ave sa longitude et latitude
+
     for(var i = 0; i <= this.capteur.length; i++){
     const myIcon = L.icon({
-    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png',
+    iconSize: [ 25, 41 ],
+    iconAnchor: [ 12, 41 ],
+    popupAnchor:[ 0, -41 ],
     });
-    L.marker([this.capteur[i].longitude, this.capteur[i].latitude], {icon: myIcon}).bindPopup(this.capteur[i].nom).addTo(myfrugalmap).openPopup();
+    L.marker([this.capteur[i].longitude, this.capteur[i].latitude], {icon: myIcon}).bindPopup(this.capteur[i].nom).addTo(this.myfrugalmap).openPopup();
 
     console.log("ajouter le marqueur pour capteur "+this.capteur[i].nom);
     }
-    
+  }
+  /*
+  flyto(){
+    this.voir = true;
+    for(var i = 0; i <= this.capteur.length; i++){
+    L.map(
+    }
+
   }
   
-
+*/
   supprimer(nom) {
     console.log("le capteur à supprimer est : "+ nom);
     for(var i = 0; i <= this.capteur.length; i++){
@@ -76,7 +100,7 @@ export class ConsultationCapteurComponent implements OnInit {
       }
     }
   }
-
+// test
   modification(){
     this.modifier = true;
   }
--- src/app/formulaire-capteur/formulaire-capteur.component.ts
+++ src/app/formulaire-capteur/formulaire-capteur.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Capteur } from '/Users/relesse/Documents/angular-camera/src/app/capteur';
 import { StockCapteur } from '/Users/relesse/Documents/angular-camera/src/app/stock-capteur';
 import { Location } from '@angular/common';
+import * as L from 'leaflet';
+import * as ELG from 'esri-leaflet-geocoder';
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
 @Component({
   selector: 'app-formulaire-capteur',
@@ -22,6 +25,25 @@ export class FormulaireCapteurComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    var myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
+    L.control.scale().addTo(myfrugalmap);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: 'données © OpenStreetMap/ODbL - rendu OSM France',
+    }).addTo(myfrugalmap);
+
+    var searchControl = new ELG.Geosearch().addTo(myfrugalmap);
+
+    var results = new L.LayerGroup().addTo(myfrugalmap);
+    
+    searchControl.on("results", function(data) {
+      results.clearLayers();
+      for (var i = data.results.length - 1; i >= 0; i--) {
+        results.addLayer(L.marker(data.results[i].latlng));
+      }
+    });
   }
 
   ajouter() {
